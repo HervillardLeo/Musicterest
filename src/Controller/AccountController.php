c5c6d01c62f76ba37e516ff20fc8f29e0c0c015d
Leo <leo.hervillard@consultants-solutec.fr>
2024-02-02 15:08:46 +0100

    Add edit account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,11 +19,20 @@ class AccountController extends AbstractController
         ]);
     }
 
-    #[Route('/account', name: 'app_edit_account')]
-    public function edit(): Response
+    #[Route('/account/edit', name: 'app_account_edit')]
+    public function edit(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('account/show.html.twig', [
-            'controller_name' => 'AccountController',
+        $user = $this->getUser();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Account updated sucessfully');
+            return $this->redirectToRoute('app_account');
+        }
+        return $this->render('account/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
